40aca440f0e3f87ddf5a460ce96fc732ff95e5ab
Luca Carnessale <lcarness@chicagobooth.edu>
2019-05-14 15:46:14 +0000

    Test 2

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -5,6 +5,9 @@ class PicturesController < ApplicationController
         render("/pic_templates/recent.html.erb")
     end
 
-
-
+    def show_details
+        photo_id = params.fetch("pic_id")
+        @picture = Photo.where({:id => photo_id}).at(0)
+        render("pic_templates/details.html.erb")
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   match("/recent",{:controller => "pictures", :action =>"latest", :via => "get"})
+  match("/photos/:pic_id",{:controller => "pictures", :action => "show_details", :via => "get"})
 end
